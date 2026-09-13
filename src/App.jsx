08257f6dcf640aad33403d0d0f15099861e570c8
enzozhouhk-work <enzozhouhk@gmail.com>
2026-09-13 18:44:08 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Frontend from './Frontend';
+import Admin    from './Admin';
+
+export default function App() {
+  const [view, setView] = useState('frontend');
+
+  return (
+    <>
+      {view === 'frontend' && <Frontend onEnterAdmin={() => setView('admin')} />}
+      {view === 'admin'    && <Admin    onBack={() => setView('frontend')} />}
+    </>
+  );
+}
